4d649c87756c49684dd9ff43cc0744ddf0927802
LDuong <lacduongldg212@gmail.com>
2024-11-09 01:47:14 +0700

    Add BrandController, addOrEdit.html for Brand

--- src/main/java/com/fashionweb/Controllers/admin/BrandController.java
+++ src/main/java/com/fashionweb/Controllers/admin/BrandController.java
@@ -2,16 +2,13 @@ package com.fashionweb.Controllers.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/admin/products")
-public class ProductController {
-
+@RequestMapping("/admin/brands")
+public class BrandController {
     @GetMapping("add")
     String add(){
-        return "admin/products/addOrEdit";
-       //return "admin/test";
+        return "admin/brands/addOrEdit";
     }
 }
