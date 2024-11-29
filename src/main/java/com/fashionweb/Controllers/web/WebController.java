b4c0124f3cca318bb880dee023a022522bd919d4
Vi021 <hellno83737@gmail.com>
2024-11-29 22:08:37 +0700

    thêm getmapping vào controller cho các file check_out.html, order_detail.html, shopping_cart.html

---
+++ src/main/java/com/fashionweb/Controllers/web/WebController.java
@@ -0,0 +1,39 @@
+package com.fashionweb.Controllers.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class WebController {
+    @GetMapping
+    String home() {
+        return "web/home";
+    }
+
+    @GetMapping("/shop")
+    String shop() {
+        return "web/shop/shop_content";
+    }
+
+    @GetMapping("/product-detail")
+    String productDetail() {
+        return "web/shop/product_detail";
+    }
+
+    @GetMapping("/order-detail")
+    String orderDetail() {
+        return "web/shop/order_detail";
+    }
+
+    @GetMapping("/check-out")
+    String checkOut() {
+        return "web/shop/check_out";
+    }
+
+    @GetMapping("/shopping-cart")
+    String shoppingCart() {
+        return "web/shop/shopping_cart";
+    }
+}
