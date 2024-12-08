e45a6b6fa11f43f2144dc42cb6c4f8c13366b480
TRayyyyyyy <vanthuong0917@gmail.com>
2024-12-09 01:44:38 +0700

    update brand

--- src/main/java/com/fashionweb/Controllers/admin/DiscountController.java
+++ src/main/java/com/fashionweb/Controllers/admin/DiscountController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller
-@RequestMapping("/admin")
+@RequestMapping("/manager")
 public class DiscountController {
     @Autowired
     private IDiscountService discountService;
